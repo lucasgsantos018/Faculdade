f3f862b83ca7c171ea405328b83348914732a19e
Lucas Gomes <lucasgsantos018@gmail.com>
2021-08-04 10:56:40 -0300

    Upando arquivos TP

--- Segundo Periodo/OrganizaçãoComputadores/TP2/principal.c
+++ Segundo Periodo/OrganizaçãoComputadores/TP2/principal.c
@@ -6,7 +6,7 @@
 #define tamcache1 16
 #define tamcache2 32
 #define tamcache3 64
-#define tamdasinstrucoes 1
+#define tamdasinstrucoes 100
 
 
 typedef struct {
@@ -47,62 +47,10 @@ int main() {
     iniciacache1(cache1);
     iniciacache2(cache2);
     iniciacache3(cache3);
-
-    for (int i = 0; i < tamcache1; i++) {
-        printf("ENDERECO NA CACHE1: %d---- ", cache1[i].endereco);
-        printf("CONTEUDO NA CACHE1: %d -----", cache1[i].conteudo);
-        printf("ESSE EH O USO NA CACHE1: %d\n", cache1[i].uso);
-    }
-
-    for (int i = 0; i < tamcache2; i++) {
-        printf("ENDERECO NA CACHE2: %d---- ", cache2[i].endereco);
-        printf("CONTEUDO NA CACHE2: %d-----", cache2[i].conteudo);
-        printf("ESSE EH O USO NA CACHE2: %d\n", cache2[i].uso);
-
-    }
-
-    for (int i = 0; i < tamcache3; i++) {
-        printf("ENDERECO NA CACHE3: %d---- ", cache3[i].endereco);
-        printf("CONTEUDO NA CACHE3: %d-----", cache3[i].conteudo);
-        printf("ESSE EH O USO NA CACHE3: %d\n", cache3[i].uso);
-
-    }
-
-    
-
-    
-
-
-
     instrucao lerinstrucoes[tamdasinstrucoes];
-
     inicialerinstrucoes(lerinstrucoes);
-
     maquinainterpretada (cache1, cache2, cache3, lerinstrucoes, ram , &cacheHit , &cacheMiss , &custoTotal);
-
     imprimirRelatorio(cacheHit,cacheMiss,custoTotal);
-
-
-    for (int i = 0; i < tamcache1; i++) {
-        printf("ENDERECO NA CACHE1: %d---- ", cache1[i].endereco);
-        printf("CONTEUDO NA CACHE1: %d -----", cache1[i].conteudo);
-        printf("ESSE EH O USO NA CACHE1: %d\n", cache1[i].uso);
-    }
-
-    for (int i = 0; i < tamcache2; i++) {
-        printf("ENDERECO NA CACHE2: %d---- ", cache2[i].endereco);
-        printf("CONTEUDO NA CACHE2: %d-----", cache2[i].conteudo);
-        printf("ESSE EH O USO NA CACHE2: %d\n", cache2[i].uso);
-    }
-
-    for (int i = 0; i < tamcache3; i++) {
-        printf("ENDERECO NA CACHE3: %d---- ", cache3[i].endereco);
-        printf("CONTEUDO NA CACHE3: %d-----", cache3[i].conteudo);
-        printf("ESSE EH O USO NA CACHE3: %d\n", cache3[i].uso);
-
-    }
-
-
     return 0;
 }
 
@@ -152,8 +100,8 @@ void inicialerinstrucoes (instrucao *instrucao) {
     srand(time(NULL));
     for (int i = 0; i < tamdasinstrucoes; i++) {
         instrucao[i].opcode = rand() % 2; 
-        instrucao[i].end1 = 1059;
-        instrucao[i].end2 = 1008;
+        instrucao[i].end1 = rand() % 1112;
+        instrucao[i].end2 = rand() % 1112;
         instrucao[i].end3 = rand() % 1112;
     }
 }
@@ -162,53 +110,63 @@ void maquinainterpretada (blocodememoria *cache1, blocodememoria *cache2, blocod
     int n , k ;
     for (int i = 0; i < tamdasinstrucoes; i++) {
         if (instrucao[i].opcode == 0) { //soma
-            printf("%d ----- ESSE AQUI E O ENDERECO QUE A GENTE TA BUSCANDO PRA SOMA 1\n", instrucao[i].end1);
-            printf("%d ----- ESSE AQUI E O ENDERECO QUE A GENTE TA BUSCANDO PRA SOMA 2\n", instrucao[i].end2);
             mudancaDeValor(cache1, cache2, cache3, instrucao[i].end1, ram , cacheHit , cacheMiss , custoTotal, 0);
-            for (int c = 0; c < 16; c++) {
+            for (int c = 0; c < tamcache1; c++) {
                 if (instrucao[i].end1 == cache1[c].endereco) {
                     n = cache1[c].conteudo;
                 }
             }
             mudancaDeValor(cache1, cache2, cache3, instrucao[i].end2, ram , cacheHit , cacheMiss , custoTotal, 0);
-            for (int c = 0; c < 16; c++) {
+            for (int c = 0; c < tamcache1; c++) {
                 if (instrucao[i].end2 == cache1[c].endereco) {
                     k = cache1[c].conteudo;
                 }
             }
-            cache1[eh_trocado(cache1, 1)].conteudo = n + k;
-            printf("Soma: %d + %d = %d\n\n",n , k , cache1[eh_trocado(cache1, 1)].conteudo);
+            
+            mudancaDeValor(cache1, cache2, cache3, instrucao[i].end3, ram , cacheHit , cacheMiss , custoTotal, 0);
+            for (int c = 0; c < tamcache1; c++) {
+                if (instrucao[i].end3 == cache1[c].endereco) {
+                    cache1[c].conteudo = n+k;
+                    printf("Soma: %d + %d = %d\n\n",n , k , cache1[c].conteudo);
+                    break;
+                }
+            }
+            //cache1[eh_trocado(cache1, 1)].conteudo = n + k;
+            
         }
         if (instrucao[i].opcode == 1) { //subtracao
-            printf("%d ----- ESSE AQUI E O ENDERECO QUE A GENTE TA BUSCANDO PRA SOMA 1\n", instrucao[i].end1);
-            printf("%d ----- ESSE AQUI E O ENDERECO QUE A GENTE TA BUSCANDO PRA SOMA 2\n", instrucao[i].end2);
             mudancaDeValor(cache1, cache2, cache3, instrucao[i].end1, ram , cacheHit , cacheMiss , custoTotal, 0);
-            for (int c = 0; c < 16; c++) {
+            for (int c = 0; c < tamcache1; c++) {
                 if (instrucao[i].end1 == cache1[c].endereco) {
                     n = cache1[c].conteudo;
                 }
             }
             mudancaDeValor(cache1, cache2, cache3, instrucao[i].end2, ram , cacheHit , cacheMiss , custoTotal, 0);
-            for (int c = 0; c < 16; c++) {
+            for (int c = 0; c < tamcache1; c++) {
                 if (instrucao[i].end2 == cache1[c].endereco) {
                     k = cache1[c].conteudo;
                 }
             }
+
+
             cache1[eh_trocado(cache1, 1)].conteudo = n - k;
             printf("Subtracao: %d - %d = %d\n\n",n , k , cache1[eh_trocado(cache1, 1)].conteudo); 
 
         }
     }
+    
+    
 }
 
 
 
 int mudancaDeValor (blocodememoria *cache1, blocodememoria *cache2, blocodememoria *cache3, int endereco, blocodememoria *ram, int *cacheHit , int *cacheMiss, int *custoTotal, int rec) { 
+    //cache1
     if((endereco >= 1000 && endereco < 1016) || rec == 1)
     {
         for(int i = 0; i < tamcache1 ; i++)
         {
-            if(cache1[i].endereco == endereco)
+            if(cache1[i].endereco == endereco || cache1[i].refresh != 0)
             {
                 cache1[i].uso++;
                 *cacheHit = *cacheHit + 1;
@@ -218,6 +176,7 @@ int mudancaDeValor (blocodememoria *cache1, blocodememoria *cache2, blocodememor
         }
     }
 
+    //cache2
    else if((endereco >= 1016 && endereco < 1048) || rec == 2)
    {
        for(int j = 0 ; j < tamcache2 ; j++)
@@ -235,13 +194,11 @@ int mudancaDeValor (blocodememoria *cache1, blocodememoria *cache2, blocodememor
                    }
                }
                *custoTotal = *custoTotal + 20; 
-               int valor =  mudancaDeValor(cache1,cache2,cache3, enderecotrocado,ram , cacheHit , cacheMiss , custoTotal, 1);
-               return valor;
+               return mudancaDeValor(cache1,cache2,cache3, endereco,ram , cacheHit , cacheMiss , custoTotal, 1);
            }
        }
    }
-
-
+    //cache3
    else if ((endereco >= 1048 && endereco < 1112) || rec == 3)
    {
        for(int k = 0 ; k < tamcache3 ; k++)
@@ -259,55 +216,35 @@ int mudancaDeValor (blocodememoria *cache1, blocodememoria *cache2, blocodememor
                    }
                }
                *custoTotal = *custoTotal + 30; 
-               int valor =  mudancaDeValor(cache1,cache2,cache3, enderecotrocado,ram , cacheHit , cacheMiss , custoTotal, 2);
-               return valor;
+               return mudancaDeValor(cache1,cache2,cache3, endereco,ram , cacheHit , cacheMiss , custoTotal, 2);
            }
        }
    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //ram
    else if (endereco >= 0 && endereco < 1000)
    {
-       for(int l = tamcache1 + tamcache2 + tamcache3 ; l < tamanhoram ; l++)          
+       for(int l = 0 ; l < tamanhoram ; l++)          
        {
            if(ram[l].endereco == endereco)
            {
-                blocodememoria aux;
-                aux = cache3[eh_trocado(cache3, 3)];
-                cache3[eh_trocado(cache3, 3)] = ram[l];
-                ram[l] = aux;
-
-               
+               blocodememoria aux;
+               int enderecotrocado = eh_trocado(cache3, 3);
+               for (int i = 0; i < 64; i++) {
+                   if (enderecotrocado == cache3[i].endereco) {
+                       aux = cache3[i];
+                       cache3[i] = ram[l];
+                       ram[l] = aux;
+                   }
+               }
                *cacheMiss = *cacheMiss + 1;
                *custoTotal = *custoTotal + 1000;
-               *cacheHit = *cacheHit - 1;
-               mudancaDeValor(cache1,cache2,cache3,eh_trocado(cache3, 3),ram , cacheHit , cacheMiss , custoTotal, 0);   
+               *cacheHit = *cacheHit - 1; 
+               return mudancaDeValor(cache1,cache2,cache3, endereco,ram , cacheHit , cacheMiss , custoTotal, 3);
            }
        }
    }
-
-
-
+   return 0;
 }
 
 
@@ -324,7 +261,7 @@ int eh_trocado (blocodememoria *blocogenerico, int qualbloco) {
         if (temcomuso0 == 0) {
             for (int i = 0; i < 16; i++) {
                 if (blocogenerico[i].uso < menor) {
-                    menor = blocogenerico[i].uso;
+                    menor = blocogenerico[i].endereco;
                 }
                 return menor;
             }
@@ -341,7 +278,7 @@ int eh_trocado (blocodememoria *blocogenerico, int qualbloco) {
         if (temcomuso0 == 0) {
             for (int i = 0; i < 32; i++) {
                 if (blocogenerico[i].uso < menor) {
-                    menor = blocogenerico[i].uso;
+                    menor = blocogenerico[i].endereco;
                 }
                 return menor;
             }
@@ -351,11 +288,14 @@ int eh_trocado (blocodememoria *blocogenerico, int qualbloco) {
     if (qualbloco == 3) {
         for (int i = 0; i < 64; i++) {
             if (blocogenerico[i].uso == 0) {
+                temcomuso0++;
                 return blocogenerico[i].endereco;
             }
-            else {
+        }  
+        if (temcomuso0 == 0) {
+            for (int i = 0; i < 64; i++) {
                 if (blocogenerico[i].uso < menor) {
-                    menor = blocogenerico[i].uso;
+                    menor = blocogenerico[i].endereco;
                 }
                 return menor;
             }
@@ -369,3 +309,6 @@ void imprimirRelatorio (int cacheHit , int cacheMiss , int custoTotal)
     printf("|Cache Miss:  %d|   ", cacheMiss);
     printf("|Custo Total: %d|\n\n", custoTotal);
 }
+
+//trocar forma de receber a resposta
+//arrumar as trocas de caches
